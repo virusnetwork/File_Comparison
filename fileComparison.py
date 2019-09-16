ba9da133c7ed9b512cf24099243587c03aa5f38e
Miles Singleton <milessingleton1212@gmail.com>
2019-09-16 09:30:47 +0100

    added comments and todo to program

--- fileComparison.py
+++ fileComparison.py
@@ -1,7 +1,12 @@
+#! python3
+# fileComparison.py - compares all files in a directory and all files in sub-directorys
+# and identify duplicate files
 import os
 import filecmp
 
 
+# returns a list of all files in directory and sub-directorys
+# takes directory location as parameter
 def getListOfFiles(dirName):
     # create a list of file and sub directories
     # names in the given directory
@@ -19,14 +24,23 @@ def getListOfFiles(dirName):
 
     return allFiles
 
+# compares all files in given list of files
+
 
 def compareFiles(listOfFiles):
-    copyFiles = list()
+    # list of all duplicate files
+    duplicateFilesList = list()
+    # loops all files in listOfFiles twice as we compare each of them
     for files in listOfFiles:
         for files2 in listOfFiles:
+            # if a duplicate is found and its not all ready in the same file list
+            # it adds to the duplicate list
             if filecmp.cmp(files, files2) and not os.path.samefile(files, files2):
-                if files in copyFiles or files2 in copyFiles:
+                if files in duplicateFilesList or files2 in duplicateFilesList:
                     continue
                 else:
                     print("%s is a copy of %s" % (files, files2))
-                    copyFiles.append(files)
+                    duplicateFilesList.append(files)
+
+# TODO: check if arguments have been given else ask user to give location
+# TODO: add main function loop that ask user to add give directory location
